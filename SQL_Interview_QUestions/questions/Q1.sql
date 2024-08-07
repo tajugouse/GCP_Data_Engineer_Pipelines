a44cb98cca8c09d970b39496d24ba9d08990d81a
Shaik Gousemohiddeen <91738153+tajugouse@users.noreply.github.com>
2024-08-07 10:02:41 +0530

    Update Q1.sql

--- SQL_Interview_QUestions/questions/Q1.sql
+++ SQL_Interview_QUestions/questions/Q1.sql
@@ -15,4 +15,18 @@ select * from friends;
 select * from packages;
 
 
-## return students names whose friends are earning more salary than them.
+-- return students names whose friends are earning more salary than them.
+
+-- First: lets find student information with first join p.id= f.id
+-- second lets find freinds informatuon with self join p1.id = f.firiend_id
+-- from cte lets collect all inof and join with student table to fetch student info cte.student_id = s.id
+
+with cte as (
+  select p.id as student_id, f.friend_id, p.salary as student_salary, p1.salary as friend_salary
+  from packages p
+  join friends f on p.id = f.id
+  join packages p1 on f.friend_id = p1.id
+)
+select cte.student_id, cte.friend_id, cte.student_salary, cte.friend_salary, s.*
+from cte
+join students s on cte.student_id = s.id where friend_salary > student_salary;
