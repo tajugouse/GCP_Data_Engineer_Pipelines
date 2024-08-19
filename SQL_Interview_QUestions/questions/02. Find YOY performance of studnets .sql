213d174be7d0864f040174e60ce8977c53a5b8cd
Shaik Gousemohiddeen <91738153+tajugouse@users.noreply.github.com>
2024-08-19 13:52:04 +0530

    commit

--- SQL_Interview_QUestions/questions/02. Find YOY performance of studnets .sql
+++ SQL_Interview_QUestions/questions/02. Find YOY performance of studnets .sql
@@ -12,7 +12,10 @@ insert into student_marks_report values('Raghav', 2021,87),('Vivek', 2021,90),
 --With the help of the lag function, we can find the previous year’s marks and then we can find the ratio of difference between current and previous to previous marks. Therefore we can find the change marks percentage if we multiply it with 100.
 
 with cte as( 
-select student_name, attempt_year, marks, lag(marks) over(partition by student_name order by attempt_year)
+select student_name, attempt_year, marks, 
+lag(marks) over(partition by student_name order by attempt_year)
 as previous_marks from student_marks_report)
 
-select student_name,attempt_year as current_year, marks as current_marks, previous_marks, ((marks-previous_marks)*100/previous_marks) as YOY_performance from cte order by student_name,attempt_year;
+select student_name,attempt_year as current_year, 
+marks as current_marks, previous_marks, 
+((marks-previous_marks)*100/previous_marks) as YOY_performance from cte order by student_name,attempt_year;
